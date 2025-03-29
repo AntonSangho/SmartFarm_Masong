da8f59257f942f60a650f4ba06b0a8b0b9ac382f
AntonSangho <sanghoemail@gmail.com>
2025-03-29 10:46:41 +0900

    네오픽셀 코드 간소화

--- src/Examples/Neopixel.py
+++ src/Examples/Neopixel.py
@@ -1,15 +1,3 @@
-"""
-네오픽셀을 켜고 끄는 의사코드.
-1. 네오픽셀 핀 설정
-2. 네오픽셀 함수 정수
-3. 무한 루프
-    4. 네오픽셀 켜기
-    5. 1초 대기
-    6. 네오픽셀 끄기
-    7. 1초 대기
-
-"""
-
 from machine import Pin
 import time
 from neopixel import NeoPixel
