61b25008c5adcd75a677c95bc54d0ade7ac948f8
AntonSangho <sanghoemail@gmail.com>
2025-04-07 10:46:21 +0900

    온도를 화시에서 섭씨로 변경

--- src/Examples/AHT20.py
+++ src/Examples/AHT20.py
@@ -7,7 +7,7 @@ i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400_000)
 sensor = ahtx0.AHT20(i2c)
 
 while True:
-    fahrenheit = sensor.temperature * 9 / 5 + 32
-    print("\nTemperature: %0.2f F" % fahrenheit)
+    celsius = sensor.temperature  # 센서에서 이미 섭씨 온도를 제공하므로 변환 불필요
+    print("\nTemperature: %0.2f C" % celsius)
     print("Humidity: %0.2f %%" % sensor.relative_humidity)
     sleep(1)
